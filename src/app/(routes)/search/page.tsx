1b43015edfcaa3a7e1d5fe742e07f3003e52d910
HOT-DOG <rightl2053@gmail.com>
2026-02-05 23:52:26 +0500

    fixed

--- src/app/(routes)/search/page.tsx
+++ src/app/(routes)/search/page.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import { Search } from "./Search";
-import { Suspense } from "react";
+//import { Search } from "./Search";
+//import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Search",
 };
 
 export default function ResultsPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Search />
-    </Suspense>
-  );
+  return "Search";
+  //<Suspense fallback={<div>Loading...</div>}>
+  //  <Search />
+  //</Suspense>
 }
